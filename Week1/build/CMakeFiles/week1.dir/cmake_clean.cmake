cdbce6196d8f090d494fe94f15c17dbd76ccb617
ssh245724 <ssh245724@gmail.com>
2023-08-17 23:03:32 +0200

    Added cmake

---
+++ Week1/build/CMakeFiles/week1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week1.dir/MyStack.cpp.o"
+  "CMakeFiles/week1.dir/MyStack.cpp.o.d"
+  "CMakeFiles/week1.dir/test.cpp.o"
+  "CMakeFiles/week1.dir/test.cpp.o.d"
+  "week1"
+  "week1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/week1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Week1/build/CMakeFiles/week1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for week1.
+# This may be replaced when dependencies are built.
