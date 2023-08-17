3fd52af6a3dd74fc0d4cffe6195fe7aba3c2fab9
ssh245724 <ssh245724@gmail.com>
2023-08-17 22:42:19 +0200

    stack class update

--- Week1/MyStack.cpp
+++ Week1/MyStack.cpp
@@ -5,100 +5,105 @@
 using namespace std;
 
 MyStack::MyStack()
-	: mStackCount(0)
-	, mStackSize(8)
-	, mStack(nullptr) 
-{
-	mStack = new int[mStackSize];
+    : mStackCount(0), mStackSize(8), mStack(nullptr) {
+    mStack = new int[mStackSize];
 }
 
 MyStack::MyStack(size_t stackSize)
-	: mStackCount(0)
-	, mStackSize(stackSize)
-	, mStack(nullptr) 
-{
-	mStack = new int[mStackSize];
+    : mStackCount(0), mStackSize(stackSize), mStack(nullptr) {
+    mStack = new int[mStackSize];
 }
 
 MyStack::~MyStack() {
-	if (mStack) delete[] mStack; mStack = nullptr;
+    if (mStack) delete[] mStack;
+    mStack = nullptr;
 }
 
 void MyStack::push(int num) {
-	mStack[mStackCount] = num;
-	++mStackCount;
-	
-	cout << "mStackCount: " << mStackCount << endl;
-	
-	if (mStackCount == mStackSize) {
-		int* backup_p = mStack;
-		if (nullptr == (mStack = static_cast<int*>(realloc(mStack, 2 * sizeof(int) * mStackSize)))) {
-			mStack = backup_p;
-			cout << "Stack size can not be extended." << endl;
-		} else {
-			mStackSize *= 2;
-		}
-	}
-		
-	cout << "mStackSize: " << mStackSize << endl;
-	cout << "push result: ";
-	for (size_t i = 0; i < mStackCount; ++i) {
-		cout << mStack[i] << " ";
-	}
-	cout <<endl << endl;
+    mStack[mStackCount] = num;
+    ++mStackCount;
+
+    cout << "mStackCount: " << mStackCount << endl;
+
+    if (mStackCount == mStackSize) {
+        int* backup_p = mStack;
+        if (nullptr == (mStack = static_cast<int*>(realloc(mStack, 2 * sizeof(int) * mStackSize)))) {
+            mStack = backup_p;
+            cout << "Stack size can not be extended." << endl;
+        } else {
+            mStackSize *= 2;
+        }
+    }
+
+    cout << "mStackSize: " << mStackSize << endl;
+    cout << "push result: ";
+    for (size_t i = 0; i < mStackCount; ++i) {
+        cout << mStack[i] << " ";
+    }
+    cout << endl
+         << endl;
 }
 
 int MyStack::pop() {
-	if (!isEmpty()) --mStackCount;
-	
-	cout << "mStackCount: " << mStackCount << endl;
-	cout << "mStackSize: " << mStackSize << endl;
-	cout << "pop result: ";
-	
-	for (size_t i = 0; i < mStackCount; ++i) {
-		cout << mStack[i] << " ";
-	}
-	cout << endl << endl;
-	
-	return mStack[mStackCount];
+    if (!isEmpty()) --mStackCount;
+
+    cout << "mStackCount: " << mStackCount << endl;
+    cout << "mStackSize: " << mStackSize << endl;
+    cout << "pop result: ";
+
+    for (size_t i = 0; i < mStackCount; ++i) {
+        cout << mStack[i] << " ";
+    }
+    cout << endl
+         << endl;
+
+    return mStack[mStackCount];
 }
 
 bool MyStack::search(int num) {
-	bool isFound = false;
-	
-	int* tmp = new int[mStackCount];
-	int tmpCount = 0;
-	
-	size_t idx = mStackCount - 1;
-	while (idx < mStackCount) {
-		int popNum = pop();
-		if (popNum == num) {
-			isFound = true;
-			for (size_t i = 0; i < tmpCount; ++i) {
-				push(tmp[i]);
-			}
-		} else {
-			tmp[tmpCount] = popNum;
-			++tmpCount;
-			--idx;
-		}
-	}
-	
-	delete[] tmp; tmp = nullptr;
-	
-	cout << "search result(" << num <<"): " << isFound <<endl;
-	cout << "mStackCount: " << mStackCount << endl;
-	cout << "mStackSize: " << mStackSize << endl << endl;
-	return isFound;
+    cout << "search starts" << endl;
+    bool isFound = false;
+
+    int* tmp = new int[mStackCount];
+    size_t tmpCount = 0;
+
+    size_t idx = mStackCount - 1;
+    while (idx < mStackCount) {
+        int popNum = pop();
+        if (popNum == num) {
+            isFound = true;
+            push(popNum);
+            break;
+        } else {
+            tmp[tmpCount] = popNum;
+            ++tmpCount;
+            --idx;
+        }
+    }
+
+    for (size_t i = tmpCount - 1; i < tmpCount; --i) {
+        push(tmp[i]);
+    }
+
+    delete[] tmp;
+    tmp = nullptr;
+
+    cout << "search result(" << num << "): " << isFound << endl;
+    cout << "mStackCount: " << mStackCount << endl;
+    cout << "mStackSize: " << mStackSize << endl;
+    cout << "search ends" << endl
+         << endl;
+    return isFound;
 }
 
 void MyStack::clear() {
-	mStackCount = 0;
-	cout << "clear all elements in stack" << endl;
-	cout << "mStackCount: " << mStackCount << endl;
-	cout << "mStackSize: " << mStackSize << endl << endl;
+    mStackCount = 0;
+    cout << "clear all elements in stack" << endl;
+    cout << "mStackCount: " << mStackCount << endl;
+    cout << "mStackSize: " << mStackSize << endl
+         << endl;
 }
 
 bool MyStack::isEmpty() {
-	return mStackCount == 0;
+    return mStackCount == 0;
 }
--- Week1/test.cpp
+++ Week1/test.cpp
@@ -1,32 +1,32 @@
-#include "MyStack.h"
-
 #include <iostream>
+
+#include "MyStack.h"
 
 using namespace std;
 
 int main(void) {
-	MyStack stack1;
-	
-	stack1.push(10);
-	stack1.push(20);
-	stack1.push(30);	
-	stack1.push(40);
-	stack1.push(50);
-	stack1.push(60);
-	stack1.push(70);	
-	stack1.push(80);
-	stack1.push(90);
-	stack1.push(100);
-	
-	stack1.pop();
-	stack1.pop();
-	stack1.pop();
-	stack1.pop();
-	
-	stack1.search(10);
-	stack1.search(70);
-	
-	stack1.clear();
-	
-	return 0;
+    MyStack stack1;
+
+    stack1.push(10);
+    stack1.push(20);
+    stack1.push(30);
+    stack1.push(40);
+    stack1.push(50);
+    stack1.push(60);
+    stack1.push(70);
+    stack1.push(80);
+    stack1.push(90);
+    stack1.push(100);
+
+    stack1.pop();
+    stack1.pop();
+    stack1.pop();
+    stack1.pop();
+
+    stack1.search(10);
+    stack1.search(70);
+
+    stack1.clear();
+
+    return 0;
 }
